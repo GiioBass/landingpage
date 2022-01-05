38cabe748dbd495dd7f6716e20ab53535da50c10
Geovanny <williamnavarro@emision.co>
2022-01-05 18:09:23 -0500

    update model quotes

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Quote extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'car_type_id',
+        'name',
+        'email',
+        'number_phone',
+        'department_id',
+        'municipality_id',
+        'policies'
+    ];
 }
